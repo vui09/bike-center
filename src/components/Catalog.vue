51f384c442e084dc291f5a961ccefcaf1f9ad04b
Julia V <vid090293@yandex.ru>
2023-05-29 21:39:31 +0300

    Верстка категорий

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -3,16 +3,36 @@
     <div class="catalog-wrapper wrapper">
       <div class="catalog__category">
         <div class="catalog__item">
+          <div class="catalog__item-wrap">
+            <a href="/" class="catalog__link" title="Велосипеды">
+              <div class="catalog__category-image">
+                <img src="../assets/images/bicycle.png" alt="Велосипеды">
+              </div>
+              <span>Велосипеды</span>
+            </a>
+          </div>
+        </div>
+        <div class="catalog__item">
           <a href="/" class="catalog__link" title="Велосипеды">
             <div class="catalog__category-image">
-              <img src="" alt="">
+              <img src="../assets/images/bicycle.png" alt="Велосипеды">
             </div>
             <span>Велосипеды</span>
           </a>
         </div>
       </div>
-      <div class="catalog__subcategory">
-
+      <div class="catalog-sub">
+        <div class="catalog-sub__item">
+          <a href="/" class="catalog-sub__link" title="Женские">Женские</a>
+          <ul class="catalog-sub__list">
+            <li>
+              <a href="/" class="catalog-sub__title-sub" title="Горные">Горные</a>
+            </li>
+            <li>
+              <a href="/" class="catalog-sub__title-sub" title="Городские">Городские</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
